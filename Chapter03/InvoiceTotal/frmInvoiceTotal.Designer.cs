0db83ca5bd5ab21ef244ea1187af77f6170763f9
James Willhoite <Jameswillhoite@gmail.com>
2019-09-05 21:24:03 -0400

    Added code snippets to the calculate button and exit button

--- Chapter03/InvoiceTotal/frmInvoiceTotal.Designer.cs
+++ Chapter03/InvoiceTotal/frmInvoiceTotal.Designer.cs
@@ -124,6 +124,7 @@
             this.btnCalculate.TabIndex = 2;
             this.btnCalculate.Text = "&Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
             // 
             // contextMenuStrip1
             // 
@@ -139,6 +140,7 @@
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
             // frmInvoiceTotal
             // 
