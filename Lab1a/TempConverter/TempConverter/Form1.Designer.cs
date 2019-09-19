8ed3aa521a3f84d3899d5e834970396a0b3a3382
James Willhoite <Jameswillhoite@gmail.com>
2019-09-19 15:29:14 -0400

    feat(lab1a): Finished

---
+++ Lab1a/TempConverter/TempConverter/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TempConverter
+{
+    partial class TemperatureConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConversion = new System.Windows.Forms.TextBox();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFahrenheit = new System.Windows.Forms.Button();
+            this.btnCelsius = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConversion
+            // 
+            this.txtConversion.Location = new System.Drawing.Point(113, 114);
+            this.txtConversion.Name = "txtConversion";
+            this.txtConversion.ReadOnly = true;
+            this.txtConversion.Size = new System.Drawing.Size(104, 20);
+            this.txtConversion.TabIndex = 1;
+            this.txtConversion.TabStop = false;
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Location = new System.Drawing.Point(113, 62);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(100, 20);
+            this.txtTemperature.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Temperature";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Conversion";
+            // 
+            // btnFahrenheit
+            // 
+            this.btnFahrenheit.Location = new System.Drawing.Point(40, 202);
+            this.btnFahrenheit.Name = "btnFahrenheit";
+            this.btnFahrenheit.Size = new System.Drawing.Size(75, 23);
+            this.btnFahrenheit.TabIndex = 2;
+            this.btnFahrenheit.Text = "&Fahrenheit";
+            this.btnFahrenheit.UseVisualStyleBackColor = true;
+            this.btnFahrenheit.Click += new System.EventHandler(this.BtnFahrenheit_Click);
+            // 
+            // btnCelsius
+            // 
+            this.btnCelsius.Location = new System.Drawing.Point(138, 202);
+            this.btnCelsius.Name = "btnCelsius";
+            this.btnCelsius.Size = new System.Drawing.Size(75, 23);
+            this.btnCelsius.TabIndex = 3;
+            this.btnCelsius.Text = "&Celsius";
+            this.btnCelsius.UseVisualStyleBackColor = true;
+            this.btnCelsius.Click += new System.EventHandler(this.BtnCelsius_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(92, 242);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(67, 22);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Cl&ear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // TemperatureConverter
+            // 
+            this.AcceptButton = this.btnFahrenheit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 292);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCelsius);
+            this.Controls.Add(this.btnFahrenheit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.txtConversion);
+            this.Name = "TemperatureConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Temperature Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConversion;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFahrenheit;
+        private System.Windows.Forms.Button btnCelsius;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Lab1a/TempConverter/TempConverter/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConverter
+{
+    public partial class TemperatureConverter : Form
+    {
+       
+        public TemperatureConverter()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void BtnFahrenheit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal celsius = 0m;
+                //Make sure the user entered a number and not text
+                if(!decimal.TryParse(txtTemperature.Text, out celsius))
+                {
+                    MessageBox.Show("Not a valid Temperature", "Invalid");
+                    txtTemperature.Text = "";
+                    txtConversion.Text = "";
+                    txtTemperature.Focus();
+                    return;
+                }
+
+
+
+                txtConversion.Text = ((celsius * 9/5) + 32).ToString();
+                
+            }
+            catch(DivideByZeroException)
+            {
+                MessageBox.Show("Divide By Zero!", "Error");
+                return;
+            }
+            catch (ArithmeticException)
+            {
+                MessageBox.Show("Here an Arithmetic Exception Occured", "Error");
+                return;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Here somehting Bad happened here.", "Exception");
+                return;
+            }
+
+        }
+
+        private void BtnCelsius_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal fahrenheit = 0m;
+                //Make sure the user entered a number and not text
+                if (!decimal.TryParse(txtTemperature.Text, out fahrenheit))
+                {
+                    MessageBox.Show("Not a valid Temperature", "Invalid");
+                    txtTemperature.Text = "";
+                    txtConversion.Text = "";
+                    txtTemperature.Focus();
+                    return;
+                }
+
+
+
+                txtConversion.Text = ((fahrenheit - 32) *  5 / 9).ToString();
+
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Divide By Zero!", "Error");
+                return;
+            }
+            catch (ArithmeticException)
+            {
+                MessageBox.Show("Here an Arithmetic Exception Occured", "Error");
+                return;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Here somehting Bad happened here.", "Exception");
+                return;
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtConversion.Text = "";
+            txtTemperature.Text = "";
+        }
+    }
+}
