c3478d690c7b99cbb5b82f88aa37dcaefbdcd313
James Willhoite <Jameswillhoite@gmail.com>
2019-09-30 21:30:16 -0400

    Student Selected will sum up totals

---
+++ Lab2/StudentScores/StudentScores/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace StudentScores
+{
+    public partial class frmStudentScores : Form
+    {
+        /*
+         * Define Variables to be used
+         */
+        protected SortedList<int, Student> students = new SortedList<int, Student>(3);
+        private int studentCount = 0;
+        public frmStudentScores()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        protected void clearForm()
+        {
+            txtAverage.Text = "";
+            txtScoreCount.Text = "";
+            txtScoreTotal.Text = "";
+        }
+
+        protected void addStudent(Student student)
+        {
+            students.Add(studentCount, student);
+
+            lbxStudents.Items.Insert(studentCount, student);
+
+            studentCount++;
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmStudentScores_Load(object sender, EventArgs e)
+        {
+            Student student1 = new Student();
+            student1.name = "John Smith";
+            double[] scores = { 98, 58, 78 };
+            student1.addScore(scores);
+            addStudent(student1);
+
+            Student student2 = new Student();
+            student2.name = "Jane Smith";
+            double[] scores2 = { 100, 100, 99 };
+            student2.addScore(scores2);
+            addStudent(student2);
+
+            Student student3 = new Student();
+            student3.name = "Bob Jones";
+            double[] scores3 = { 38, 40, 15 };
+            student3.addScore(scores3);
+            addStudent(student3);
+        }
+
+        private void LbxStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Student student;
+            try
+            {
+                student = students[lbxStudents.SelectedIndex];
+                //Sum up total Score
+                double score_total = 0;
+                int score_count = 0;
+                double score_average = 0;
+                foreach (double score in student.scores)
+                {
+                    score_total += score;
+                    score_count++;
+                }
+
+                score_average = score_total / score_count;
+
+                txtAverage.Text = String.Format("{0:F3}", score_average);
+                txtScoreCount.Text = score_count.ToString();
+                txtScoreTotal.Text = String.Format("{0:F0}" ,score_total);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Selected Student is out of Range.", "Out of range");
+            }
+
+            
+        }
+    }
+}
---
+++ Lab2/StudentScores/StudentScores/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentScores
+{
+    public class Student
+    {
+        public string name = "";
+        public List<double> scores = new List<double>(0);
+
+        public void addScore(double score)
+        {
+            scores.Add(score);
+        }
+
+        public void addScore(Array array_scores)
+        {
+            foreach(double score in array_scores)
+            {
+                scores.Add(score);
+            }
+        }
+
+        public void updateScore(int index, double score)
+        {
+            try
+            {
+                scores[index] = score;
+            } catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Could not update this Score.", "Not Found");
+                return;
+            }
+        }
+
+        public void removeScore(int index)
+        {
+            try
+            {
+                scores.Remove(index);
+            } catch(IndexOutOfRangeException)
+            {
+                MessageBox.Show("Could not remove this score from " + name, "Unable to remove");
+                return;
+            }
+        }
+
+        public override string ToString()
+        {
+            string text = name + " ";
+            foreach(double score in scores)
+            {
+                text += "|" + score;
+            }
+
+            return text;
+        }
+    }
+}
