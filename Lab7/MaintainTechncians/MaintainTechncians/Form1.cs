78a038eba19b04dece7d1bb2d3d4a7c0b64abd11
James Willhoite <Jameswillhoite@gmail.com>
2019-11-21 21:58:15 -0500

    feat(lab7): Form Layout Complete

---
+++ Lab7/MaintainTechncians/MaintainTechncians/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaintainTechncians
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TechniciansBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.techniciansBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.techSupportDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'techSupportDataSet.Technicians' table. You can move, or remove it, as needed.
+            this.techniciansTableAdapter.Fill(this.techSupportDataSet.Technicians);
+
+        }
+    }
+}
