18396ebe3b33da6f8e02c097d7d9e7a53b3e94aa
jbwillhoite <jameswillhoite@gmail.com>
2019-12-06 09:57:19 -0500

    saving

--- Lab7.1/MaintTech/MaintTech/Form1.cs
+++ Lab7.1/MaintTech/MaintTech/Form1.cs
@@ -8,26 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MaintainTechncians
+namespace MaintTech
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void TechniciansBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.techniciansBindingSource.EndEdit();
-
-            //DataRow[] row = this.techSupportDataSet.Technicians.Select("TechID = '" + techIDTextBox.Text + "'");
-            //Console.WriteLine(row[0][3] + ", " +phoneTextBox.Text);
-            //row[0][3] = phoneTextBox.Text;
-            //this.techSupportDataSet.Tables[0].AcceptChanges();
-            this.tableAdapterManager.UpdateAll(this.techSupportDataSet);
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
